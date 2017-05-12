eba6aac6d7e93ebadb22c76967a736d5aee3a452
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-05-11 22:59:11 -0700

    1st set of files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,41 @@
 /**
  * Created by ryanhoyda on 5/11/17.
  */
+//Variables
+
+//array strings
+var wordBank = ["gargoyle", "demon", "ghost", "godzilla", "imp", "giant", "gorgon",
+    "pegasus", "ghoul", "horror", "ogre", "vampire", "werewolf", "yeti"];
+//string
+var currentWord;
+//string
+var userGuess;
+//string
+var compGuess;
+//string
+var lettersGuessed;
+//integer
+var numGuesses = currentWord * 1.5;
+//integer
+var numWins;
+//integer
+var numLosses;
+
+var currentWordArray = [];
+
+
+function start(){
+    numGuesses = 0;
+    //random num generator
+    var index = Math.floor((Math.random() * wordBank.length));
+    currentWord = wordBank[index].toLowerCase();
+    currentWordArray = [];
+    for (var i = 0; i < currentWord.length; i++){
+        currentWordArray.push('_');
+    }
+}
+
+
+document.onkeyup = function(event) {
+
+}
